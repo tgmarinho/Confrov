57f660f56c53d204267c42543a3c9fc239e20ca0
Thiago Marinho <tgmarinho@gmail.com>
2013-09-07 16:28:13 -0400

    criacao da entidade Marca

--- src/main/java/br/edu/pos/confrov/entity/Marca.java
+++ src/main/java/br/edu/pos/confrov/entity/Marca.java
@@ -1,7 +1,9 @@
 package br.edu.pos.confrov.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -10,9 +12,62 @@ import javax.persistence.Table;
 public class Marca {
 
 	@Id
-	@GeneratedValue()
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Long id;
 	
+	@Column(name = "descricao")
 	private String descricao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	@Override
+	public String toString() {
+		return descricao;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Marca other = (Marca) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+ 
+	
+	
+	
 	
 }
