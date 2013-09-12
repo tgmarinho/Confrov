4abe7cad7e8256a85d6c8b9787c7659d984575b4
Thiago Marinho <tgmarinho@gmail.com>
2013-09-12 00:26:12 -0400

    classe Veiculo

---
+++ src/main/java/br/edu/pos/confrov/entity/Veiculo.java
@@ -0,0 +1,136 @@
+package br.edu.pos.confrov.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+@Entity
+@SequenceGenerator(name="seq_veiculo", sequenceName="seq_veiculo", initialValue=1, allocationSize=1)
+@Table(name="tb_veiculo", uniqueConstraints=@UniqueConstraint(columnNames="ca_descricao"))
+@NamedQueries({
+    @NamedQuery(name = "Categoria.findAll", query = "SELECT c FROM Categoria c"),
+    @NamedQuery(name = "Categoria.findByDescricao", query = "SELECT c FROM Categoria c WHERE c.descricao = :descricao"),
+    @NamedQuery(name = "Categoria.findById", query = "SELECT c FROM Categoria c WHERE c.id = :id")})
+public class Veiculo implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	
+	private Long id;
+	
+	private String placa;
+	
+	private Integer anoFabricacao;
+	
+	private Integer anoModelo;
+	
+	private String cor;
+	
+	private Marca marca;
+	
+	private Modelo modelo;
+	
+	private Categoria categoria;
+	
+	private Double valorLocacao;
+
+	public String getPlaca() {
+		return placa;
+	}
+
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+	public Marca getMarca() {
+		return marca;
+	}
+
+	public void setMarca(Marca marca) {
+		this.marca = marca;
+	}
+
+	public Modelo getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(Modelo modelo) {
+		this.modelo = modelo;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public Double getValorLocacao() {
+		return valorLocacao;
+	}
+
+	public void setValorLocacao(Double valorLocacao) {
+		this.valorLocacao = valorLocacao;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public Integer getAnoFabricacao() {
+		return anoFabricacao;
+	}
+
+	public void setAnoFabricacao(Integer anoFabricacao) {
+		this.anoFabricacao = anoFabricacao;
+	}
+
+	public Integer getAnoModelo() {
+		return anoModelo;
+	}
+
+	public void setAnoModelo(Integer anoModelo) {
+		this.anoModelo = anoModelo;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Veiculo other = (Veiculo) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	
+}
