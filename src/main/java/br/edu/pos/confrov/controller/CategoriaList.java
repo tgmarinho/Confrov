628eaa5ac7e71661f3359b89eadd73876d46cde9
Thiago Marinho <tgmarinho@gmail.com>
2013-09-14 17:45:08 -0400

    alterando o managed bean

--- src/main/java/br/edu/pos/confrov/controller/CategoriaList.java
+++ src/main/java/br/edu/pos/confrov/controller/CategoriaList.java
@@ -1,13 +1,31 @@
 package br.edu.pos.confrov.controller;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import org.apache.log4j.Logger;
+
+import br.edu.pos.confrov.entity.Categoria;
 
 
 @ManagedBean(name="cateoriaList")
+@ViewScoped
 public class CategoriaList {
 
+	private static final Logger log = Logger.getLogger(CategoriaList.class);
+
+	private Categoria categoria;
+
 	
-	
-	
-	
+	public void salvar(){
+		
+		
+		
+		
+	}
+
+
+
+
+
 }
